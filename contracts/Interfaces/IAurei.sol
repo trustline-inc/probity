03f0a5af8ff7d0cd7f20e299ace3bc4276f973c2
mattrosendin <matthew.rosendin@gmail.com>
2021-02-24 14:46:18 -0800

    style: add doxygen comment

--- contracts/Interfaces/IAurei.sol
+++ contracts/Interfaces/IAurei.sol
@@ -5,14 +5,17 @@ pragma solidity ^0.8.0;
 import "../Dependencies/IERC20.sol";
 import "../Dependencies/IERC2612.sol";
 
+/**
+ * @notice Common interface for the Aurei token.
+ */
 interface IAurei is IERC20, IERC2612 {
   // --- Events ---
+
   event AureiBalanceUpdated(address _user, uint _amount);
   event VaultManagerAddressChanged(address _vaultManagerAddress);
-  event StabilityPoolAddressChanged(address _newStabilityPoolAddress);
-  event BorrowerOperationsAddressChanged(address _newBorrowerOperationsAddress);
 
   // --- Functions ---
+
   function mint(address _account, uint256 _amount) external;
   function burn(address _account, uint256 _amount) external;
 }
