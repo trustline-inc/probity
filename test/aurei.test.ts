6634efc45d04dd1d76b24a75281daa7ba65fe399
mattrosendin <matthew.rosendin@gmail.com>
2021-03-03 09:48:08 -0800

    clean up tests

--- test/aurei.test.ts
+++ test/aurei.test.ts
@@ -1,33 +1,21 @@
 import "@nomiclabs/hardhat-waffle";
 import { ethers } from "hardhat";
-// We import Chai to use its asserting functions here.
 import { expect } from "chai";
 
-// `describe` is a Mocha function that allows you to organize your tests. It's
-// not actually needed, but having your tests organized makes debugging them
-// easier. All Mocha functions are available in the global scope.
+describe("Aurei contract", function() {
 
-// `describe` receives the name of a section of your test suite, and a callback.
-// The callback must define the tests of that section. This callback can't be
-// an async function.
-describe("Aurie contract", function() {
-  // Mocha has four functions that let you hook into the the test runner's
-  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
-
-  // They're very useful to setup the environment for tests, and to clean it
-  // up after they run.
-
-  // A common pattern is to declare some variables, and assign them in the
-  // `before` and `beforeEach` callbacks.
-
-  let Teller;
-  let teller;
-  let Treasury;
-  let treasury;
+  // Contracts
   let Aurei;
+  let Teller;
+  let Treasury;
+
+  // Instances
   let aurei;
+  let teller;
+  let treasury;
+  
+  // Wallets
   let owner;
-  let aureiTokenOwnerAddress;
   let addr1;
   let addr2;
   let addrs;
@@ -41,9 +29,9 @@ describe("Aurie contract", function() {
     await teller.deployed();
 
     Aurei = await ethers.getContractFactory("Aurei");
-    //aureiTokenOwnerAddress = teller.address;
-    aureiTokenOwnerAddress = owner.address;
-    aurei = await Aurei.deploy(aureiTokenOwnerAddress);
+    // const aureiOwnerAddress = teller.address;
+    const aureiOwnerAddress = owner.address;
+    aurei = await Aurei.deploy(aureiOwnerAddress);
     await aurei.deployed();
     
     Treasury = await ethers.getContractFactory("Treasury");
@@ -51,14 +39,10 @@ describe("Aurie contract", function() {
     await treasury.deployed();
     
   });
-  describe("Deployment", function () {
-    // `it` is another Mocha function. This is the one you use to define your
-    // tests. It receives the test name, and a callback function.
 
-    // If the callback function is async, Mocha will `await` it.
+  describe("Deployment", function () {
+
     it("Should set the right owner", async function () {
-      // Expect receives a value, and wraps it in an Assertion object. These
-      // objects have a lot of utility methods to assert values.
       expect(await aurei.owner()).to.equal(aureiTokenOwnerAddress);
     });
 
@@ -73,21 +57,25 @@ describe("Aurie contract", function() {
   });
   
   describe("Transactions", function () {
+
     it("Minting new tokens and verify owner balance and token supply", async function () {
       await treasury.mint(100);
       const ownerBalance = await aurei.balanceOf(aureiTokenOwnerAddress);
       expect(ownerBalance).to.equal(100);
       expect(await aurei.totalSupply()).to.equal(100);
     });
+
     it("Burning Tokens and verify owner balance and token supply", async function () {
       await treasury.burn(20);
       const ownerBalance = await aurei.balanceOf(aureiTokenOwnerAddress);
       expect(ownerBalance).to.equal(80);
       expect(await aurei.totalSupply()).to.equal(80);
     });
+
     it("Transfer tokens to another address", async function () {
       await aurei.transfer(addr1.address, 20);
       expect(await aurei.balanceOf(addr1.address)).to.equal(20);
     });
+
   });   
 });
