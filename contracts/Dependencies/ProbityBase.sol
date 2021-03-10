ce69c1f9b576893e43b3ab8f013903ba66aa06da
mattrosendin <matthew.rosendin@gmail.com>
2021-03-09 23:34:16 -0800

    remove TOKEN_MULTIPLIER (duplicate) and updates

--- contracts/Dependencies/ProbityBase.sol
+++ contracts/Dependencies/ProbityBase.sol
@@ -36,15 +36,13 @@ contract ProbityBase {
   // One as 1e18, or as 100%
   uint256 constant ONE = 10**18;
 
-  uint256 constant TOKEN_MULTIPLIER = 10**18;
-
   // Minimum collateral ratio for individual vaults (150%)
   uint256 public constant MIN_COLLATERAL_RATIO = 1500000000000000000;
 
-  //Seconds in year for APY calulcation
-  uint256 public constant MPR_MULTIPLIER = 31536000; //Seconds in year: 365*24*3600;
+  // Seconds in year: 365 * 24 * 3600;
+  uint256 public constant SECONDS_IN_YEAR = 31536000;
 
-  //Todo: Define unit system for aurei
+  // TODO: Define unit system for Aurei
   function ray(uint256 wad) internal pure returns (uint256) {
     return wad * 10**9;
   }
