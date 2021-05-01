7b9cf266ed290677e1c2d190e48f22d8e6ca0fa7
mattrosendin <matthew.rosendin@gmail.com>
2021-05-01 16:15:59 -0700

    update aurei bridge demo initiation script

--- scripts/aurei.ts
+++ scripts/aurei.ts
@@ -1,16 +1,24 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import "@nomiclabs/hardhat-waffle";
 import { ethers } from "hardhat";
-
-const bridgeContractAbi = require("../artifacts/contracts/Bridge.sol/Bridge.json");
 const aureiContractAbi = require("../artifacts/contracts/Aurei.sol/Aurei.json");
+const bridgeContractAbi = require("../artifacts/contracts/Bridge.sol/Bridge.json");
+const aureiContractAddress = "0x32dfE359aa1E100Ba8CfBF5f38BcBdb3f55e8a06";
+const bridgeContractAddress = "0xb6f0184c26DBDe79E19325259f79f8eB0B07aAD6";
+const receiverXrpAddress = "rNeVJZtx4HxgTjT41VVPW3VPHJSz3Gf38R";
 
-const bridgeContractAddress = "0x3E3578908883aff83F46d001a2c2b8A4fE3782e5";
-const aureiContractAddress = "0x1be91Ab0e8D679985086CE9AC493A02f92FB3FE2";
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-async function main(xrpAddress) {
+/**
+ * @function main
+ * @param xrpAddress
+ */
+async function main(xrpAddress: string) {
   let owner: SignerWithAddress;
-  [owner] = await ethers.getSigners();
+  let personal: SignerWithAddress;
+  [owner, personal] = await ethers.getSigners();
 
   const bridgeContract = new ethers.Contract(
     bridgeContractAddress,
@@ -23,15 +31,36 @@ async function main(xrpAddress) {
     owner
   );
 
-  // fund 100 Aurei to ownerAddress
-  await aureiContract.mint(owner.address, 3000000);
+  // fund 3000000 Aurei to personal address
+  await aureiContract.mint(personal.address, 3000000);
+  await sleep(5000);
 
   // increase allowance from owner to the bridgeContract
-  await aureiContract.increaseAllowance(bridgeContract.address, 3000000);
+  const aureiContractPersonal = aureiContract.connect(personal);
+  await aureiContractPersonal.increaseAllowance(bridgeContract.address, 30);
+  await sleep(5000);
 
-  // timestamp now in seconds
+  // check allowance
+  const allowance = (
+    await aureiContract.allowance(personal.address, bridgeContract.address)
+  ).toNumber();
+  console.log("bridge contract allowance:", allowance);
+
+  // transfer the aurei to the xrpl
+  const bridgeContractPersonal = bridgeContract.connect(personal);
   const nonce = Math.floor(Date.now() / 1000);
-  await bridgeContract.transferAureiToXRP(xrpAddress, 3000000, nonce);
-}
+  await bridgeContractPersonal.transferAureiToXRP(xrpAddress, 30, nonce);
+  await sleep(5000);
 
-main("rpP1zrii6LGidTtLpgz8Xtx9mHi35LzMEL");
+  // check for transfers
+  console.log(
+    "xrp transfers:",
+    await bridgeContractPersonal.getToXRPTransferHashes()
+  );
+  await sleep(5000);
+
+  // get tx hash from above and pass in below
+  // console.log(await bridgeContractPersonal.toXRPTransfers('0x4a22e9451ec0365fe33cf0ee7eaaad063cc014b2beeef61c80714784dfa34eb5'))
+  // console.log((await aureiContract.allowance(personal.address, bridgeContract.address)).toNumber())
+}
+main(receiverXrpAddress);
