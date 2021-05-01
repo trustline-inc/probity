bc8efbe2affcc129ded0e82837902746c5a2f5ca
mattrosendin <matthew.rosendin@gmail.com>
2021-05-01 13:26:14 -0700

    get local accounts from flare config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,7 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-web3";
 import "@nomiclabs/hardhat-waffle";
 import { HardhatUserConfig } from "hardhat/config";
+const flare = require("../flare/client/config.json");
 
 const config: HardhatUserConfig = {
   defaultNetwork: "coston",
@@ -18,7 +19,7 @@ const config: HardhatUserConfig = {
     },
     local: {
       url: "http://127.0.0.1:9650/ext/bc/C/rpc",
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [flare.accounts[0].privateKey, flare.accounts[1].privateKey],
       chainId: 16,
     },
     private: {
