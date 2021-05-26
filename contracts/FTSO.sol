369d73220fc954124cbd54311edf49cc8fce5ca9
mattrosendin <matthew.rosendin@gmail.com>
2021-05-26 11:55:46 -0700

    add basic ftso contract

---
+++ contracts/FTSO.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Dependencies/Base.sol";
+import "hardhat/console.sol";
+
+/**
+ * @notice Mock contract to store and set FLR/USD prices.
+ */
+contract FTSO {
+  // --- Data ---
+
+  uint256 price;
+
+  // --- Constructor ---
+
+  constructor() {}
+
+  // --- External Functions ---
+
+  function setPrice(uint256 _price) external {
+    price = _price;
+  }
+
+  function getPrice() external view returns (uint256) {
+    return price;
+  }
+}
