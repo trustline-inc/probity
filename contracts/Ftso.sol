3959ff91607cc07216832bf29fb062b907e924b8
mattrosendin <matthew.rosendin@gmail.com>
2021-06-03 14:26:16 -0700

    create ftso interface

--- contracts/Ftso.sol
+++ contracts/Ftso.sol
@@ -3,16 +3,13 @@
 pragma solidity ^0.8.0;
 
 import "./Dependencies/Base.sol";
+import "./Interfaces/IFtso.sol";
 import "hardhat/console.sol";
 
 /**
  * @notice Mock contract to store and set FLR/USD prices.
  */
-contract Ftso {
-  // --- Events ---
-
-  event PriceUpdated(uint256 _price);
-
+contract Ftso is IFtso {
   // --- Data ---
 
   uint256 price;
@@ -25,12 +22,12 @@ contract Ftso {
 
   // --- External Functions ---
 
-  function setPrice(uint256 _price) external {
+  function setPrice(uint256 _price) external override {
     price = _price;
     emit PriceUpdated(_price);
   }
 
-  function getPrice() external view returns (uint256) {
+  function getPrice() external view override returns (uint256) {
     return price;
   }
 }
