36fcd8e00db87f7ea10889a5c8a56bb7f73e037a
shine <aungshine@gmail.com>
2021-07-27 10:35:50 -0700

    Collateral for FLR and ERC20 Tokens

---
+++ contracts/probity/collateral/ERC20Collateral.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../../Dependencies/Stateful.sol";
+import "../../Interfaces/ICollateral.sol";
+
+interface VaultLike {
+  function modifyCollateral(
+    bytes32 collateral,
+    address user,
+    int256 amount
+  ) external;
+}
+
+interface TokenLike {
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+}
+
+contract ERC20Collateral is Stateful, ICollateral {
+  bytes32 collateralId;
+  VaultLike vault;
+  TokenLike collateralToken;
+
+  constructor(
+    address registryAddress,
+    bytes32 collateralHash,
+    TokenLike collateral,
+    VaultLike vaultAddress
+  ) Stateful(registryAddress) {
+    collateralId = collateralHash;
+    vault = vaultAddress;
+    collateralToken = collateral;
+  }
+
+  function deposit(uint256 amount) external onlyWhen("paused", false) {
+    require(
+      collateralToken.transferFrom(msg.sender, address(this), amount),
+      "ERC20_COLL: transfer failed"
+    );
+    vault.modifyCollateral(collateralId, msg.sender, int256(amount));
+  }
+
+  function withdraw(uint256 amount) external onlyWhen("paused", false) {
+    require(
+      collateralToken.transfer(msg.sender, amount),
+      "ERC20_COLL: transfer failed"
+    );
+    vault.modifyCollateral(collateralId, msg.sender, -int256(amount));
+  }
+}
---
+++ contracts/probity/collateral/FLRCollateral.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../../Dependencies/Stateful.sol";
+import "../../Interfaces/ICollateral.sol";
+
+interface VaultLike {
+  function modifyCollateral(
+    bytes32 collateral,
+    address user,
+    int256 amount
+  ) external;
+}
+
+contract FLRCollateral is Stateful, ICollateral {
+  bytes32 collateralId;
+  VaultLike vault;
+
+  constructor(
+    address registryAddress,
+    bytes32 collateralHash,
+    VaultLike vaultAddress
+  ) Stateful(registryAddress) {
+    collateralId = collateralHash;
+    vault = vaultAddress;
+  }
+
+  function deposit() external payable onlyWhen("paused", false) {
+    vault.modifyCollateral(collateralId, msg.sender, int256(msg.value));
+  }
+
+  function withdraw(uint256 amount) external onlyWhen("paused", false) {
+    require(payable(msg.sender).send(amount), "FLR_COLL: fail to send FLR");
+    vault.modifyCollateral(collateralId, msg.sender, -int256(amount));
+  }
+}
