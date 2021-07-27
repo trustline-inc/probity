bd36619fa6751c9b00bbedc6658e17fb0fd95296
shine <aungshine@gmail.com>
2021-07-27 10:14:10 -0700

    Vault + dependencies

---
+++ contracts/Dependencies/Stateful.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./AccessControl.sol";
+
+contract Stateful is AccessControl {
+  mapping(bytes32 => bool) states;
+
+  modifier onlyWhen(bytes32 name, bool set) {
+    require(states[name] == set, "State check failed");
+    _;
+  }
+
+  constructor(address registryAddress) AccessControl(registryAddress) {}
+
+  function setState(bytes32 name, bool set) external onlyBy("gov") {
+    states[name] = set;
+  }
+}
