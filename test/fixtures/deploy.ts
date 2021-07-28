953a2e76f7750887b61b2959adea9fd4ca035f1d
shine <aungshine@gmail.com>
2021-07-27 23:08:50 -0700

    happy path test

--- test/fixtures/deploy.ts
+++ test/fixtures/deploy.ts
@@ -1,19 +1,25 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import "@nomiclabs/hardhat-waffle";
-import { BigNumber } from "ethers";
-import { ethers } from "hardhat";
+import { ethers, web3 } from "hardhat";
 
 // Import contract factory types
 import {
   AureiFactory,
   BridgeFactory,
-  FtsoFactory,
   RegistryFactory,
-  TcnTokenFactory,
-  TellerFactory,
-  TreasuryFactory,
   VaultFactory,
   StateConnectorFactory,
+  FlrCollateralFactory,
+  Erc20CollateralFactory,
+  TellerFactory,
+  TreasuryFactory,
+  FtsoFactory,
+  PriceFeedFactory,
+  AuctioneerFactory,
+  LinearDecreaseFactory,
+  LiquidatorFactory,
+  ReservePoolFactory,
+  Erc20TokenFactory,
 } from "../../typechain";
 
 // Import contract types
@@ -23,10 +29,18 @@ import {
   Ftso,
   Registry,
   TcnToken,
-  Teller,
-  Treasury,
   Vault,
   StateConnector,
+  FlrCollateral,
+  Erc20Collateral,
+  Teller,
+  Treasury,
+  PriceFeed,
+  Auctioneer,
+  LinearDecrease,
+  Liquidator,
+  ReservePool,
+  Erc20Token,
 } from "../../typechain";
 
 /**
@@ -38,10 +52,18 @@ interface Contracts {
   ftso: Ftso;
   registry: Registry;
   tcnToken: TcnToken;
-  teller: Teller;
-  treasury: Treasury;
   vault: Vault;
   stateConnector: StateConnector;
+  flrCollateral: FlrCollateral;
+  fxrpCollateral: Erc20Collateral;
+  teller: Teller;
+  treasury: Treasury;
+  priceFeed: PriceFeed;
+  auctioneer: Auctioneer;
+  linearDecrease: LinearDecrease;
+  liquidator: Liquidator;
+  reserve: ReservePool;
+  erc20: Erc20Token;
 }
 
 const contracts: Contracts = {
@@ -50,10 +72,18 @@ const contracts: Contracts = {
   ftso: null,
   registry: null,
   tcnToken: null,
-  teller: null,
-  treasury: null,
   vault: null,
   stateConnector: null,
+  flrCollateral: null,
+  fxrpCollateral: null,
+  teller: null,
+  treasury: null,
+  priceFeed: null,
+  auctioneer: null,
+  linearDecrease: null,
+  liquidator: null,
+  reserve: null,
+  erc20: null,
 };
 
 // Contracts submitted to the register
@@ -91,7 +121,7 @@ const signers: Signers = {
   addrs: null,
 };
 
-const deployBridge = async () => {
+const getSigners = async () => {
   // Set signers
   [
     signers.owner,
@@ -104,6 +134,27 @@ const deployBridge = async () => {
     signers.liquidator,
     ...signers.addrs
   ] = await ethers.getSigners();
+  return signers;
+};
+
+const bytes32 = (string) => ethers.utils.formatBytes32String(string);
+
+const deployRegistry = async () => {
+  const signers = await getSigners();
+
+  const registryFactory = (await ethers.getContractFactory(
+    "Registry",
+    signers.owner
+  )) as RegistryFactory;
+  contracts.registry = await registryFactory.deploy(signers.owner.address);
+  await contracts.registry.deployed();
+
+  return contracts;
+};
+
+const deployAUR = async () => {
+  // Set signers
+  const signers = await getSigners();
 
   const aureiFactory = (await ethers.getContractFactory(
     "Aurei",
@@ -111,6 +162,204 @@ const deployBridge = async () => {
   )) as AureiFactory;
   contracts.aurei = await aureiFactory.deploy();
   await contracts.aurei.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("aur"),
+    contracts.aurei.address
+  );
+
+  return contracts;
+};
+
+const deployVault = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const vaultFactory = (await ethers.getContractFactory(
+    "Vault",
+    signers.owner
+  )) as VaultFactory;
+  contracts.vault = await vaultFactory.deploy(contracts.registry.address);
+  await contracts.vault.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("vault"),
+    contracts.vault.address
+  );
+
+  return contracts;
+};
+
+const deployCollateral = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const flrCollateralFactory = (await ethers.getContractFactory(
+    "FLRCollateral",
+    signers.owner
+  )) as FlrCollateralFactory;
+  contracts.flrCollateral = await flrCollateralFactory.deploy(
+    contracts.registry.address,
+    web3.utils.keccak256("FLR Collateral"),
+    contracts.vault.address
+  );
+  await contracts.flrCollateral.deployed();
+
+  const fxrpCollateralFactory = (await ethers.getContractFactory(
+    "ERC20Collateral",
+    signers.owner
+  )) as Erc20CollateralFactory;
+  contracts.fxrpCollateral = await fxrpCollateralFactory.deploy(
+    contracts.registry.address,
+    web3.utils.keccak256("FXRP Collateral"),
+    contracts.erc20.address,
+    contracts.vault.address
+  );
+  await contracts.fxrpCollateral.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("collateral"),
+    contracts.flrCollateral.address
+  );
+  await contracts.registry.setupContractAddress(
+    bytes32("collateral"),
+    contracts.fxrpCollateral.address
+  );
+
+  return contracts;
+};
+
+const deployFtso = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const ftsoFactory = (await ethers.getContractFactory(
+    "Ftso",
+    signers.owner
+  )) as FtsoFactory;
+  contracts.ftso = await ftsoFactory.deploy();
+  await contracts.ftso.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("ftso"),
+    contracts.ftso.address
+  );
+
+  return contracts;
+};
+
+const deployTeller = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const tellerFactory = (await ethers.getContractFactory(
+    "Teller",
+    signers.owner
+  )) as TellerFactory;
+  contracts.teller = await tellerFactory.deploy(
+    contracts.registry.address,
+    contracts.vault.address
+  );
+  await contracts.teller.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("teller"),
+    contracts.teller.address
+  );
+
+  return contracts;
+};
+
+const deployTreasury = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const treasuryFactory = (await ethers.getContractFactory(
+    "Treasury",
+    signers.owner
+  )) as TreasuryFactory;
+  contracts.treasury = await treasuryFactory.deploy(
+    contracts.registry.address,
+    contracts.aurei.address,
+    contracts.vault.address
+  );
+  await contracts.treasury.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("treasury"),
+    contracts.treasury.address
+  );
+
+  return contracts;
+};
+
+const deployPriceFeed = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const priceFeedFactory = (await ethers.getContractFactory(
+    "PriceFeed",
+    signers.owner
+  )) as PriceFeedFactory;
+  contracts.priceFeed = await priceFeedFactory.deploy(
+    contracts.registry.address,
+    contracts.vault.address
+  );
+  await contracts.priceFeed.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("priceFeed"),
+    contracts.priceFeed.address
+  );
+
+  return contracts;
+};
+
+const deployAuction = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const auctionFactory = (await ethers.getContractFactory(
+    "Auctioneer",
+    signers.owner
+  )) as AuctioneerFactory;
+  contracts.auctioneer = await auctionFactory.deploy(
+    contracts.registry.address,
+    contracts.vault.address,
+    contracts.linearDecrease.address,
+    contracts.ftso.address
+  );
+  await contracts.auctioneer.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("auctioneer"),
+    contracts.auctioneer.address
+  );
+
+  return contracts;
+};
+
+const deployPriceCalc = async () => {
+  // Set signers
+  const signers = await getSigners();
+
+  const linearDecreaseFactory = (await ethers.getContractFactory(
+    "LinearDecrease",
+    signers.owner
+  )) as LinearDecreaseFactory;
+  contracts.linearDecrease = await linearDecreaseFactory.deploy();
+  await contracts.linearDecrease.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("priceCalc"),
+    contracts.linearDecrease.address
+  );
+
+  return contracts;
+};
+
+const deployBridge = async () => {
+  const signers = await getSigners();
 
   const stateConnectorFactory = (await ethers.getContractFactory(
     "StateConnector",
@@ -129,7 +378,95 @@ const deployBridge = async () => {
   );
   await contracts.bridge.deployed();
 
+  return contracts;
+};
+
+const deployReserve = async () => {
+  const signers = await getSigners();
+
+  const reserveFactory = (await ethers.getContractFactory(
+    "ReservePool",
+    signers.owner
+  )) as ReservePoolFactory;
+  contracts.reserve = await reserveFactory.deploy(
+    contracts.registry.address,
+    contracts.vault.address
+  );
+
+  await contracts.reserve.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("reserve"),
+    contracts.reserve.address
+  );
+
+  return contracts;
+};
+
+const deployERC20 = async () => {
+  const signers = await getSigners();
+
+  const erc20Factory = (await ethers.getContractFactory(
+    "ERC20Token",
+    signers.owner
+  )) as Erc20TokenFactory;
+  contracts.erc20 = await erc20Factory.deploy();
+
+  await contracts.erc20.deployed();
+
+  return contracts;
+};
+
+const deployLiquidator = async () => {
+  const signers = await getSigners();
+
+  const liquidatorFactory = (await ethers.getContractFactory(
+    "Liquidator",
+    signers.owner
+  )) as LiquidatorFactory;
+  contracts.liquidator = await liquidatorFactory.deploy(
+    contracts.registry.address,
+    contracts.vault.address,
+    contracts.reserve.address
+  );
+
+  await contracts.liquidator.deployed();
+
+  await contracts.registry.setupContractAddress(
+    bytes32("liquidator"),
+    contracts.liquidator.address
+  );
+
+  return contracts;
+};
+
+const deployProbity = async () => {
+  // Set signers
+  const signers = await getSigners();
+  await deployRegistry();
+  await deployAUR();
+  await deployVault();
+  await deployERC20();
+  await deployCollateral();
+  await deployFtso();
+  await deployTeller();
+  await deployPriceCalc();
+  await deployPriceFeed();
+  await deployAuction();
+  await deployTreasury();
+  await deployReserve();
+  await deployLiquidator();
+
   return { contracts, signers };
 };
 
-export { deployBridge };
+const deployBridgeSystem = async () => {
+  // Set signers
+  const signers = await getSigners();
+  await deployAUR();
+  await deployBridge();
+
+  return { contracts, signers };
+};
+
+export { deployProbity, deployBridgeSystem };
---
+++ test/happyFlow.test.ts
@@ -0,0 +1,388 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+
+import {
+  Aurei,
+  Erc20Collateral,
+  Vault,
+  FlrCollateral,
+  Teller,
+  Treasury,
+  Ftso,
+  PriceFeed,
+  Auctioneer,
+  Liquidator,
+  ReservePool,
+  Registry,
+  Erc20Token,
+} from "../typechain";
+import { deployProbity } from "./fixtures/deploy";
+import { ethers, web3 } from "hardhat";
+import * as chai from "chai";
+import { errorTypes, ADDRESS_ZERO, BYTES32_ZERO } from "./utils/constants";
+import assertRevert from "./utils/assertRevert";
+import { STATUS_CODES } from "http";
+import { keccak256 } from "ethers/lib/utils";
+const expect = chai.expect;
+
+const AMOUNT_TO_ISSUE = 10;
+// Wallets
+let redeemer: SignerWithAddress;
+let owner: SignerWithAddress;
+let user: SignerWithAddress;
+
+// Contracts
+let aurei: Aurei;
+let vault: Vault;
+let registry: Registry;
+let flrColl: FlrCollateral;
+let fxrpColl: Erc20Collateral;
+let teller: Teller;
+let treasury: Treasury;
+let ftso: Ftso;
+let priceFeed: PriceFeed;
+let auctioneer: Auctioneer;
+let liquidator: Liquidator;
+let reserve: ReservePool;
+let erc20: Erc20Token;
+
+const PRECISION_COLL = ethers.BigNumber.from("1000000000000000000");
+const PRECISION_PRICE = ethers.BigNumber.from("1000000000000000000000000000");
+const PRECISION_AUR = ethers.BigNumber.from(
+  "1000000000000000000000000000000000000000000000"
+);
+
+const COLL_AMOUNT = PRECISION_COLL.mul(1000);
+const SUPPLY_COLL_AMOUNT = PRECISION_COLL.mul(400);
+const SUPPLY_AMOUNT = PRECISION_COLL.mul(200);
+const LOAN_COLL_AMOUNT = PRECISION_COLL.mul(200);
+const LOAN_AMOUNT = PRECISION_COLL.mul(100);
+
+let flrCollId = web3.utils.keccak256("FLR Collateral");
+let fxrpCollId = web3.utils.keccak256("FXRP Collateral");
+
+describe("Probity Happy flow", function () {
+  beforeEach(async function () {
+    const { contracts, signers } = await deployProbity();
+
+    // Set contracts
+    vault = contracts.vault;
+    flrColl = contracts.flrCollateral;
+    fxrpColl = contracts.fxrpCollateral;
+    aurei = contracts.aurei;
+    teller = contracts.teller;
+    treasury = contracts.treasury;
+    ftso = contracts.ftso;
+    priceFeed = contracts.priceFeed;
+    auctioneer = contracts.auctioneer;
+    liquidator = contracts.liquidator;
+    reserve = contracts.reserve;
+    registry = contracts.registry;
+    erc20 = contracts.erc20;
+
+    owner = signers.owner;
+    user = signers.alice;
+
+    flrCollId = web3.utils.keccak256("FLR Collateral");
+  });
+
+  it("test deposit and withdrawal of collateral", async () => {
+    const WITHDRAW_AMOUNT = COLL_AMOUNT.div(3);
+
+    // FLR COLLATERAL
+    let flrBalBefore = await ethers.provider.getBalance(owner.address);
+    let vaultFlrBalBefore = await vault.vaults(flrCollId, owner.address);
+
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    let flrBalAfter = await ethers.provider.getBalance(owner.address);
+    let vaultFlrBalAfter = await vault.vaults(flrCollId, owner.address);
+    expect(flrBalBefore.sub(flrBalAfter) >= COLL_AMOUNT).to.equal(true);
+    expect(vaultFlrBalAfter[0].sub(vaultFlrBalBefore[0])).to.equal(COLL_AMOUNT);
+
+    flrBalBefore = await ethers.provider.getBalance(owner.address);
+    vaultFlrBalBefore = await vault.vaults(flrCollId, owner.address);
+
+    await flrColl.withdraw(WITHDRAW_AMOUNT);
+
+    flrBalAfter = await ethers.provider.getBalance(owner.address);
+    vaultFlrBalAfter = await vault.vaults(flrCollId, owner.address);
+    expect(flrBalBefore.sub(flrBalAfter) < WITHDRAW_AMOUNT).to.equal(true);
+    expect(vaultFlrBalBefore[0].sub(vaultFlrBalAfter[0])).to.equal(
+      WITHDRAW_AMOUNT
+    );
+
+    // FXRP COLLATERAL
+    await erc20.mint(owner.address, COLL_AMOUNT);
+    await erc20.approve(fxrpColl.address, COLL_AMOUNT);
+
+    let fxrpBalBefore = await erc20.balanceOf(owner.address);
+    let vaultFxrpBalBefore = await vault.vaults(fxrpCollId, owner.address);
+
+    await fxrpColl.deposit(COLL_AMOUNT);
+
+    let vaultFxrpBalAfter = await vault.vaults(fxrpCollId, owner.address);
+    let fxrpBalAfter = await erc20.balanceOf(owner.address);
+
+    expect(fxrpBalBefore.sub(fxrpBalAfter)).to.equal(COLL_AMOUNT);
+    expect(vaultFxrpBalAfter[0].sub(vaultFxrpBalBefore[0])).to.equal(
+      COLL_AMOUNT
+    );
+
+    fxrpBalBefore = await erc20.balanceOf(owner.address);
+    vaultFxrpBalBefore = await vault.vaults(fxrpCollId, owner.address);
+
+    await fxrpColl.withdraw(WITHDRAW_AMOUNT);
+
+    vaultFxrpBalAfter = await vault.vaults(fxrpCollId, owner.address);
+    fxrpBalAfter = await erc20.balanceOf(owner.address);
+
+    expect(fxrpBalAfter.sub(fxrpBalBefore)).to.equal(WITHDRAW_AMOUNT);
+    expect(vaultFxrpBalBefore[0].sub(vaultFxrpBalAfter[0])).to.equal(
+      WITHDRAW_AMOUNT
+    );
+  });
+
+  it("test modifySupply, modifyDebt and AUR withdrawal", async () => {
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    await vault.initCollType(flrCollId);
+    await vault.updateCeiling(flrCollId, PRECISION_AUR.mul(10000000));
+    await teller.addCollType(flrCollId);
+    await priceFeed.init(flrCollId, PRECISION_COLL.mul(150), ftso.address);
+    await priceFeed.updatePrice(flrCollId);
+
+    let userVaultBefore = await vault.vaults(flrCollId, owner.address);
+
+    await vault.modifySupply(
+      flrCollId,
+      treasury.address,
+      SUPPLY_COLL_AMOUNT,
+      SUPPLY_AMOUNT
+    );
+
+    let userVaultAfter = await vault.vaults(flrCollId, owner.address);
+    expect(userVaultBefore[0].sub(userVaultAfter[0])).to.equal(
+      SUPPLY_COLL_AMOUNT
+    );
+    expect(userVaultAfter[3].sub(userVaultBefore[3])).to.equal(SUPPLY_AMOUNT);
+
+    userVaultBefore = await vault.vaults(flrCollId, owner.address);
+    let aurBefore = await vault.aur(owner.address);
+
+    await vault.modifyDebt(
+      flrCollId,
+      treasury.address,
+      LOAN_COLL_AMOUNT,
+      LOAN_AMOUNT
+    );
+
+    let aurAfter = await vault.aur(owner.address);
+    expect(aurAfter.sub(aurBefore)).to.equal(LOAN_AMOUNT.mul(PRECISION_PRICE));
+    userVaultAfter = await vault.vaults(flrCollId, owner.address);
+    expect(userVaultBefore[0].sub(userVaultAfter[0])).to.equal(
+      LOAN_COLL_AMOUNT
+    );
+    expect(userVaultAfter[2].sub(userVaultBefore[2])).to.equal(LOAN_AMOUNT);
+  });
+
+  it("test priceFeed Update", async () => {
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    await vault.initCollType(flrCollId);
+    await vault.updateCeiling(flrCollId, PRECISION_AUR.mul(10000000));
+    await teller.addCollType(flrCollId);
+    await priceFeed.init(
+      flrCollId,
+      PRECISION_PRICE.mul(15).div(10),
+      ftso.address
+    );
+
+    await priceFeed.updatePrice(flrCollId);
+
+    let collTypeAfter = await vault.collTypes(flrCollId);
+    let expectedPrice = PRECISION_PRICE.div(3).mul(2);
+    // as long as the expectedPrice is within a buffer, call it success
+    expect(collTypeAfter[2].sub(expectedPrice).toNumber() <= 10).to.equal(true);
+  });
+
+  it("test liquidation start", async () => {
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    await vault.initCollType(flrCollId);
+    await vault.updateCeiling(flrCollId, PRECISION_AUR.mul(10000000));
+    await teller.addCollType(flrCollId);
+    await liquidator.init(flrCollId, auctioneer.address);
+    await priceFeed.init(flrCollId, PRECISION_PRICE, ftso.address);
+    await priceFeed.updatePrice(flrCollId);
+
+    await vault.modifySupply(
+      flrCollId,
+      treasury.address,
+      SUPPLY_COLL_AMOUNT,
+      SUPPLY_AMOUNT
+    );
+    await vault.modifyDebt(
+      flrCollId,
+      treasury.address,
+      LOAN_COLL_AMOUNT,
+      LOAN_AMOUNT
+    );
+
+    await priceFeed.updateMinCollRatio(
+      flrCollId,
+      PRECISION_PRICE.mul(15).div(10)
+    );
+    await priceFeed.updatePrice(flrCollId);
+
+    let unBackedAurBefore = await vault.unBackedAUR(reserve.address);
+    let userVaultBefore = await vault.vaults(flrCollId, owner.address);
+    await liquidator.liquidateVault(flrCollId, owner.address);
+    let unBackedAurAfter = await vault.unBackedAUR(reserve.address);
+    let userVaultAfter = await vault.vaults(flrCollId, owner.address);
+
+    expect(unBackedAurAfter.sub(unBackedAurBefore)).to.equal(
+      SUPPLY_AMOUNT.add(LOAN_AMOUNT).mul(PRECISION_PRICE)
+    );
+    expect(userVaultBefore[1].sub(userVaultAfter[1])).to.equal(
+      SUPPLY_COLL_AMOUNT.add(LOAN_COLL_AMOUNT)
+    );
+  });
+
+  it("test auction process", async () => {
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    await vault.initCollType(flrCollId);
+    await vault.updateCeiling(flrCollId, PRECISION_AUR.mul(10000000));
+    await teller.addCollType(flrCollId);
+    await liquidator.init(flrCollId, auctioneer.address);
+    await priceFeed.init(flrCollId, PRECISION_PRICE, ftso.address);
+    await priceFeed.updatePrice(flrCollId);
+
+    await vault.modifySupply(
+      flrCollId,
+      treasury.address,
+      SUPPLY_COLL_AMOUNT,
+      SUPPLY_AMOUNT
+    );
+    await vault.modifyDebt(
+      flrCollId,
+      treasury.address,
+      LOAN_COLL_AMOUNT,
+      LOAN_AMOUNT
+    );
+
+    await priceFeed.updateMinCollRatio(
+      flrCollId,
+      PRECISION_PRICE.mul(15).div(10)
+    );
+    await priceFeed.updatePrice(flrCollId);
+
+    await liquidator.liquidateVault(flrCollId, owner.address);
+
+    const flrCollUser = flrColl.connect(user);
+    const vaultUser = vault.connect(user);
+    const auctioneerUser = auctioneer.connect(user);
+    await flrCollUser.deposit({ value: PRECISION_COLL.mul(3000) });
+    await vaultUser.modifySupply(
+      flrCollId,
+      treasury.address,
+      PRECISION_COLL.mul(2000),
+      PRECISION_COLL.mul(1000)
+    );
+    await vaultUser.modifyDebt(
+      flrCollId,
+      treasury.address,
+      PRECISION_COLL.mul(900),
+      PRECISION_COLL.mul(600)
+    );
+
+    await auctioneerUser.placeBid(
+      0,
+      PRECISION_PRICE.mul(11).div(10),
+      PRECISION_COLL.mul("100")
+    );
+    let bidAfter = await auctioneer.bids(0, user.address);
+    expect(bidAfter[0]).to.equal(PRECISION_PRICE.mul(11).div(10));
+    expect(bidAfter[1]).to.equal(PRECISION_COLL.mul("100"));
+
+    let userVaultBefore = await vault.vaults(flrCollId, user.address);
+    let userAurBefore = await vault.aur(user.address);
+    await auctioneerUser.buyItNow(
+      0,
+      PRECISION_PRICE.mul(12).div(10),
+      PRECISION_AUR.mul("200")
+    );
+    let userVaultAfter = await vault.vaults(flrCollId, user.address);
+    let userAurAfter = await vault.aur(user.address);
+    let expectedLot = PRECISION_AUR.mul("200").div(
+      PRECISION_PRICE.mul(12).div(10)
+    );
+    expect(userAurBefore.sub(userAurAfter)).to.equal(PRECISION_AUR.mul("200"));
+    expect(
+      userVaultAfter[0].sub(userVaultBefore[0]).sub(expectedLot).toNumber() <=
+        1e17
+    ).to.equal(true);
+  });
+
+  it("test reserve pool settlement + IOU sale", async () => {
+    await flrColl.deposit({ value: COLL_AMOUNT });
+
+    await vault.initCollType(flrCollId);
+    await vault.updateCeiling(flrCollId, PRECISION_AUR.mul(10000000));
+    await teller.addCollType(flrCollId);
+    await liquidator.init(flrCollId, auctioneer.address);
+    await priceFeed.init(flrCollId, PRECISION_PRICE, ftso.address);
+    await priceFeed.updatePrice(flrCollId);
+
+    await vault.modifySupply(
+      flrCollId,
+      treasury.address,
+      SUPPLY_COLL_AMOUNT,
+      SUPPLY_AMOUNT
+    );
+    await vault.modifyDebt(
+      flrCollId,
+      treasury.address,
+      LOAN_COLL_AMOUNT,
+      LOAN_AMOUNT
+    );
+
+    await priceFeed.updateMinCollRatio(
+      flrCollId,
+      PRECISION_PRICE.mul(15).div(10)
+    );
+    await priceFeed.updatePrice(flrCollId);
+
+    await liquidator.liquidateVault(flrCollId, owner.address);
+
+    const flrCollUser = flrColl.connect(user);
+    const vaultUser = vault.connect(user);
+    await flrCollUser.deposit({ value: PRECISION_COLL.mul(3000) });
+    await vaultUser.modifySupply(
+      flrCollId,
+      treasury.address,
+      PRECISION_COLL.mul(2000),
+      PRECISION_COLL.mul(1000)
+    );
+    await vaultUser.modifyDebt(
+      flrCollId,
+      treasury.address,
+      PRECISION_COLL.mul(900),
+      PRECISION_COLL.mul(600)
+    );
+
+    await reserve.setDebtThreshold(PRECISION_AUR.mul(200));
+    await reserve.startIOUSale();
+
+    const reserveUser = reserve.connect(user);
+    await ethers.provider.send("evm_increaseTime", [21601]);
+    await ethers.provider.send("evm_mine", []);
+    await reserveUser.buyIOU(PRECISION_AUR.mul(100));
+
+    let userIOU = await reserve.ious(user.address);
+    expect(userIOU > PRECISION_AUR.mul(100)).to.equal(true);
+  });
+});
