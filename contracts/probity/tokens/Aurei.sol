bab5b3ad62b139af6926f742ccb0705f9cb9c762
Matthew Rosendin <matthew.rosendin@gmail.com>
2021-09-17 14:22:21 -0700

    fix file name

--- contracts/probity/tokens/Aurei.sol
+++ contracts/probity/tokens/Aurei.sol
@@ -200,23 +200,22 @@ contract Aurei is IAurei, Ownable {
     bytes32 s
   ) external override {
     require(deadline >= block.timestamp, "AUR: EXPIRED");
-    bytes32 digest =
-      keccak256(
-        abi.encodePacked(
-          "\x19\x01",
-          _DOMAIN_SEPARATOR,
-          keccak256(
-            abi.encode(
-              _PERMIT_TYPEHASH,
-              owner,
-              spender,
-              amount,
-              _nonces[owner]++,
-              deadline
-            )
+    bytes32 digest = keccak256(
+      abi.encodePacked(
+        "\x19\x01",
+        _DOMAIN_SEPARATOR,
+        keccak256(
+          abi.encode(
+            _PERMIT_TYPEHASH,
+            owner,
+            spender,
+            amount,
+            _nonces[owner]++,
+            deadline
           )
         )
-      );
+      )
+    );
     address recoveredAddress = ecrecover(digest, v, r, s);
     require(
       recoveredAddress != address(0) && recoveredAddress == owner,
