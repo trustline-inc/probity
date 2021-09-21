9c499c0901ce6ee5ac21a453e445e3e8252375a3
Matthew Rosendin <matt@trustline.co>
2021-09-21 15:13:06 -0700

    changes part 1

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -1,77 +1,39 @@
 // SPDX-License-Identifier: MIT
 
-/**
- * @dev Flare network <> XRPL, Aurei bridge contract
- * The purpose for this smart contract is to be able to issue the stablecoin AUR(Aurei) on XRP ledger in a
- * trustless manner backed by 1:1 with AUR on the flare network. This contract will lock up the AUR amount that is
- * to be issued in XRPL and users looking to hold AUR on XRPL should check this smart contract to verify that
- * the issuer has followed the procedure laid out below. If the issuer doesn't follow the procedure precisely,
- * they will be penalized by losing the AUR they deposited,  * and a portion of it rewarded
- * to the person that proved the misuse
- *
- *
- * The process to Issue AUR on XRPL:
- * - On XRPL, User need 2 XRPL accounts, 'issuer account' and 'receiver account'
- * - On Flare, User needs give Bridge contract (this contract) AUR allowance by amount they wish to issue on XRPL
- * - On Flare, lock up Aurei in this smart contract by calling 'newIssuer' with the XRPL issuer account address and amount
- * - On XRPL, create a trustline from 'receiver account' to 'issuer account' with 'amount' locked in bridgeContract
- * - On XRPL, set issuer account setting 'defaultRippling" to true and 'disableAuth' to false
- * - On XRPL, add regular key ACCOUNT_ONE to issuer account settings
- * - On XRPL, issue exactly the 'amount' from issuer account to receiver account
- * - On XRPL, set the issuer account setting 'disableMasterKey' to true
- * - On FLare, call stateConnector contract and prove the payment by calling 'provePaymentFinality' and provide the txHash on XRPL
- * - On Flare, call stateConnector contract and prove that the issuer account can no longer issue
- *        by calling 'TBD' (This feature has not been implemented)
- * - On Flare, call 'completeIssuance' on this contract which will complete the Issuer's issuance
- * - On Flare, call 'verifyIssuer' on this contract to verify that issuer can no longer issue
- *      anymore AUR (This can only be done after stateConnector has implemented the feature)
- *
- * The process to Redeem AUR on XRPL:
- * - On Flare, call redepmtionAttempt to pre-register an redemption attempt
- * - On XRPL, send the AUR back to the issuer by the amount you wish you redeem
- * - On Flare, call stateConnector contract and prove the payment has completed
- * - On Flare, call bridge contract's `redeemAUR` function to unlock the AUR
- *
- *
- * Shine Lee @shine2lay - shine@trustline.co
- **/
-
 pragma solidity ^0.8.0;
 
 import "./interfaces/IAurei.sol";
 import "./interfaces/IStateConnector.sol";
 
-// new version
 contract Bridge {
   /////////////////////////////////////////
   // Events
   /////////////////////////////////////////
-  event RedemptionCompleted(bytes32 indexed txHashOnXRP, uint256 amount);
 
+  event RedemptionCompleted(bytes32 indexed XrplTxId, uint256 amount);
   event IssuanceCompleted(string issuer, uint256 amount);
+
   /////////////////////////////////////////
   // Modifiers
   /////////////////////////////////////////
+
   modifier issuerExists(string calldata issuer) {
+    require(issuers[issuer].sender != address(0), "The issuer does not exist.");
+    _;
+  }
+
+  modifier issuerMustBePending(string calldata issuer) {
     require(
-      issuers[issuer].sender != address(0),
-      "This issuer does not exists"
+      issuers[issuer].status == Status.PENDING,
+      "The issuer is not in the PENDING state."
     );
     _;
   }
 
-  modifier issuerMustBeInProgress(string calldata issuer) {
-    require(
-      issuers[issuer].status == IssuerStatus.IN_PROGRESS,
-      "issuer status is not longer IN_PROGRESS"
-    );
-    _;
-  }
-
-  modifier issuerDoesNotExists(string calldata issuer) {
+  modifier issuerDoesNotExist(string calldata issuer) {
     require(
       issuers[issuer].sender == address(0),
-      "This issuer already exists "
+      "An issuer already exists with this address."
     );
     _;
   }
@@ -79,24 +41,22 @@ contract Bridge {
   /////////////////////////////////////////
   // Data Storage
   /////////////////////////////////////////
+
   /**
-   *        0 = DOES_NOT_EXIST (default value, issuer doesn't exits)
-   *        0 = IN_PROGRESS (AUR is locked but user have not issued on XRPL yet)
-   *        1 = CANCELLED (AUR is no longer held and user have cancel the issuer)
-   *        2 = COMPLETE (Issuance is complete but have not proven that they can no longer issue more AUR,
-   *               meaning the client side should validate more before accepting) **
-   *        3 = VERIFIED (no need for more validation)
-   *        4 = REDEEMED (all the AUR issued has already been REDEEMED, so this issuer is no longer valid)
-   *        5 = FRAUD (The issuer address have been verified to have more than 1 issuance thus no longer valid)
+   * 0 = PENDING    - Tokens are locked but are not issued on the XRPL)
+   * 1 = CANCELLED  - Tokens are returned the issuer is cancelled)
+   * 2 = COMPLETE   - Tokens are issued but unverified for spending
+   * 3 = VERIFIED   - Tokens are verified for spending
+   * 4 = REDEEMED   - Tokens are fully redeemed and the issuer is no longer valid
+   * 5 = FRAUDULENT - Tokens are issued but the issuer is invalid
    **/
-  enum IssuerStatus {
-    DOES_NOT_EXIST,
-    IN_PROGRESS,
+  enum Status {
+    PENDING,
     CANCELED,
-    COMPLETE,
+    COMPLETED,
     VERIFIED,
     REDEEMED,
-    FRAUD
+    FRAUDULENT
   }
 
   struct PreRedemption {
@@ -116,8 +76,8 @@ contract Bridge {
   struct Issuer {
     uint256 amount;
     address sender;
-    bytes32 txHashOnXRP;
-    IssuerStatus status;
+    bytes32 XrplTxId;
+    Status status;
   }
 
   IAurei aurei;
@@ -133,6 +93,7 @@ contract Bridge {
   /////////////////////////////////////////
   // Constructor
   /////////////////////////////////////////
+
   constructor(address aureiAddress, address stateConnectorAddress) {
     aurei = IAurei(aureiAddress);
     stateConnector = IStateConnector(stateConnectorAddress);
@@ -143,79 +104,61 @@ contract Bridge {
   /////////////////////////////////////////
 
   /**
-   * @dev Check the Issuer's status
-   *
-   * Users should call this function to check whether they should accept AUR from this issuer 
-   *
+   * @notice Call this function to check whether to accept tokens from this issuer
+   * @dev This is a stopgap before the verification is implemented in state connector contract
    * @param issuer the XRPL address of the issuer
    * @return status
-   *        0 = IN_PROGRESS (AUR is locked but user have not issued on XRPL yet)
-   *        1 = CANCELLED (AUR is no longer held and user have cancel the issuer) 
-   *        2 = COMPLETE (issuer is complete but have not proven that they can no longer issue more AUR, 
-                  meaning the client side should validate more before accepting) **
-   *        3 = VERIFIED (no need for more validation)
-   *        4 = REDEEMED (all the AUR issued has already been REDEEMED, so this issuer is no longer valid)
-   *  **this is a stop gap before the verification is implemented in stateConnector contract
-  **/
-  function checkIssuerStatus(string calldata issuer)
+   */
+  function getIssuerStatus(string calldata issuer)
     external
     view
-    returns (IssuerStatus status)
+    returns (Status status)
   {
     return issuers[issuer].status;
   }
 
   /**
-   * @dev Lock up AUR for new issuer
-   *
-   * This function locks up and AUR and allow user to issue AUR of the same amount on XRPL
-   * from the provided xrpAddress
-   *
+   * @notice Locks up tokens for XRPL issuance
    * @param issuer the XRPL address of the issuer
-   * @param amount the amount of AUR to lock up and also issue on the XRPL
+   * @param amount the amount of tokens to be issued on the XRPL
    **/
-  function newIssuer(string calldata issuer, uint256 amount)
+  function createIssuer(string calldata issuer, uint256 amount)
     external
-    issuerDoesNotExists(issuer)
+    issuerDoesNotExist(issuer)
   {
-    require(amount != 0, "amount must be a non zero amount");
+    require(amount > 0, "Amount must be greater than zero.");
     aurei.transferFrom(msg.sender, address(this), amount);
     issuers[issuer].amount = amount;
     issuers[issuer].sender = msg.sender;
-    issuers[issuer].status = IssuerStatus.IN_PROGRESS;
+    issuers[issuer].status = Status.PENDING;
   }
 
   /**
-   * @dev Cancel an issuer and release the AUR locked up associated with the issuer
-   * The user canceling the issuer must be the same user that initiated the createissuer
-   *
-   * @param issuer issuerAddress on XRP
+   * @notice Cancels a pending issuer and returns the locked tokens to the originator.
+   * @dev The caller must be originating account of the issuing address.
+   * @param issuer issuing address on the XRPL.
    **/
   function cancelIssuer(string calldata issuer)
     external
     issuerExists(issuer)
-    issuerMustBeInProgress(issuer)
+    issuerMustBePending(issuer)
   {
     require(
       issuers[issuer].sender == msg.sender,
-      "Only the original sender of the is issuer can cancel it"
+      "Only the originating account can cancel this issuer."
     );
-
-    // refund AUR to the msg.sender
     aurei.transfer(msg.sender, issuers[issuer].amount);
-
-    issuers[issuer].status = IssuerStatus.CANCELED;
+    issuers[issuer].status = Status.CANCELED;
   }
 
   /**
-   * @dev Prove that the AUR issuer on XRPL has completed so that the issuer address can be marked as valid issuer
-   *      by marking the issuer Address as COMPLETE
-   *
-   * @param txHash the payment tx ID from XRPL
-   * @param source address of the tx
-   * @param issuer address of the tx
-   * @param destinationTag of tx
-   * @param amount sent in tx
+   * @notice Prove that the XRPL issuance completed so that the issuer can be validated.
+   * @dev Marks the status of the issuer as COMPLETED
+   * @param txHash the XRPL payment tx ID
+   * @param source the address of the source - @shine2lay to clarify
+   * @param issuer the address of the issuer
+   * @param destinationTag the destination tag
+   * @param amount the amount issued
    * @param currencyHash hash of the currency code
    **/
   function completeIssuance(
@@ -225,7 +168,7 @@ contract Bridge {
     uint64 destinationTag,
     uint64 amount,
     bytes32 currencyHash
-  ) external issuerMustBeInProgress(issuer) {
+  ) external issuerMustBePending(issuer) {
     verifyPaymentFinality(
       txHash,
       source,
@@ -235,13 +178,13 @@ contract Bridge {
       currencyHash
     );
 
-    issuers[issuer].txHashOnXRP = txHash;
-    issuers[issuer].status = IssuerStatus.COMPLETE;
+    issuers[issuer].XrplTxId = txHash;
+    issuers[issuer].status = Status.COMPLETED;
     emit IssuanceCompleted(issuer, amount);
   }
 
   /**
-   * @dev every issuer address is only allowed to do One issuer, by proving the 2nd issuer from the account,
+   * @dev Every issuer address is only allowed to do One issuer, by proving the 2nd issuer from the account,
    *      you can prove that the issuer account is no longer legit and can be consider fraudulent
    *      the one who submit the proof will be given a reward from some of the AUR that has been locked in by the
    *      issuer address
@@ -262,8 +205,8 @@ contract Bridge {
     bytes32 currencyHash
   ) external {
     require(
-      issuers[issuer].txHashOnXRP != txHash,
-      "The provided txHash has already proven"
+      issuers[issuer].XrplTxId != txHash,
+      "The provided tx hash has already proven."
     );
     verifyPaymentFinality(
       txHash,
@@ -274,7 +217,7 @@ contract Bridge {
       currencyHash
     );
 
-    issuers[issuer].status = IssuerStatus.FRAUD;
+    issuers[issuer].status = Status.FRAUDULENT;
     uint256 amountToSend = issuers[issuer].amount;
     issuers[issuer].amount = 0;
     // send all of the AUR to the sender of this call as reward
@@ -376,7 +319,7 @@ contract Bridge {
     );
 
     if (issuers[issuer].amount == 0) {
-      issuers[issuer].status = IssuerStatus.REDEEMED;
+      issuers[issuer].status = Status.REDEEMED;
     }
 
     emit RedemptionCompleted(txHash, amount);
@@ -401,8 +344,8 @@ contract Bridge {
    * @dev Prove that the AUR issuer on XRPL has completed so that the issuer address can be marked as valid issuer
    *
    * @param txHash the payment tx ID from XRPL
-   * @param source address of the tx
-   * @param issuer address of the tx
+   * @param source address of the tx - @shine2lay to clarify
+   * @param issuer address of the issuer
    * @param destinationTag the issuer Tag of tx
    * @param amount sent in tx
    * @param currencyHash hash of the currency code
