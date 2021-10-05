631c8609c57bfd0873bc4da98417ed080bd94d87
shine <aungshine@gmail.com>
2021-10-05 12:13:40 -0700

    remove comments

--- contracts/test/VPToken.sol
+++ contracts/test/VPToken.sol
@@ -23,7 +23,6 @@ contract VPToken is IAurei, Ownable {
 
   // --- ERC2612 Data ---
 
-  // @dev commented out for now because of ProviderError: invalid opcode: CHAINID
   bytes32 private immutable _DOMAIN_SEPARATOR;
   bytes32 private constant _PERMIT_TYPEHASH =
     0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9; // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
@@ -36,7 +35,6 @@ contract VPToken is IAurei, Ownable {
   constructor() Ownable(msg.sender) {
     address[] storage delegates;
     uint256[] storage delegateBips;
-    // @dev commented out for now because of ProviderError: invalid opcode: CHAINID
     uint256 chainId;
     assembly {
       chainId := chainid()
