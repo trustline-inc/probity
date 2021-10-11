99f7fd436cc74b7cfbb1f0184d5f75927b7f2b09
Matthew Rosendin <matt@trustline.co>
2021-10-11 11:03:55 -0700

    create interval

--- scripts/rateUpdater.ts
+++ scripts/rateUpdater.ts
@@ -10,5 +10,14 @@ import { Contract } from "ethers";
   let [owner] = await ethers.getSigners();
   const TellerABI = await artifacts.readArtifact("Teller");
   const teller = new Contract(process.env.TELLER, TellerABI.abi, owner);
-  await teller.updateIndices(web3.utils.keccak256("FLR"), { gasLimit: 400000 });
+
+  setInterval(async () => {
+    console.log("Updating rates...");
+    const tx = await teller.updateIndices(web3.utils.keccak256("FLR"), {
+      gasLimit: 400000,
+    });
+    console.log(tx);
+    const result = await tx.wait();
+    console.log(result); // CALL_EXCEPTION
+  }, 5000);
 })();
