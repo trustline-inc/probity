1146cb4cba8fbf041212fecc1ff9159f8daeeac7
Matthew Rosendin <matt@trustline.co>
2021-11-06 20:56:29 -0700

    use currency instead of chain id

--- scripts/init.ts
+++ scripts/init.ts
@@ -6,21 +6,15 @@ import { artifacts, ethers, web3 } from "hardhat";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+if (!process.env.CURRENCY)
+  throw Error("Must sent the CURRENCY environment variable.");
+if (!["aurei", "phi"].includes(process.env.CURRENCY))
+  throw Error("Invalid currency type.");
+
 const COLLATERAL = {
   FLR: web3.utils.keccak256("FLR"),
   SGB: web3.utils.keccak256("SGB"),
 };
-
-let symbol: string;
-
-switch (Number(process.env.CHAIN_ID)) {
-  case 16:
-    symbol = "FLR";
-  case 19:
-    symbol = "SGB";
-  default:
-    symbol = "FLR";
-}
 
 const PRECISION_COLL = ethers.BigNumber.from("1000000000000000000");
 const PRECISION_AUR = ethers.BigNumber.from(
@@ -52,25 +46,34 @@ const init = async () => {
   );
 
   // // Initialize native collateral type
-  console.log(`Initializing ${symbol} collateral`);
-  await vaultEngine.connect(owner).initCollType(COLLATERAL[symbol]);
-  console.log(`Vault: ${symbol} initialized.`);
+  console.log(`Initializing ${process.env.CURRENCY} collateral`);
   await vaultEngine
     .connect(owner)
-    .updateCeiling(COLLATERAL[symbol], PRECISION_AUR.mul(10000000));
+    .initCollType(COLLATERAL[process.env.CURRENCY]);
+  console.log(`Vault: ${process.env.CURRENCY} initialized.`);
+  await vaultEngine
+    .connect(owner)
+    .updateCeiling(
+      COLLATERAL[process.env.CURRENCY],
+      PRECISION_AUR.mul(10000000)
+    );
   console.log(`Vault: ceiling updated.`);
   await teller
     .connect(owner)
-    .initCollType(COLLATERAL[symbol], 0, { gasLimit: 300000 });
-  console.log(`Teller: ${symbol} initialized.`);
+    .initCollType(COLLATERAL[process.env.CURRENCY], 0, { gasLimit: 300000 });
+  console.log(`Teller: ${process.env.CURRENCY} initialized.`);
   await priceFeed
     .connect(owner)
-    .init(COLLATERAL[symbol], PRECISION_COLL.mul(150), process.env.FTSO);
-  console.log(`PriceFeed: ${symbol} price initialized.`);
+    .init(
+      COLLATERAL[process.env.CURRENCY],
+      PRECISION_COLL.mul(150),
+      process.env.FTSO
+    );
+  console.log(`PriceFeed: ${process.env.CURRENCY} price initialized.`);
   await priceFeed
     .connect(owner)
-    .updatePrice(COLLATERAL[symbol], { gasLimit: 300000 });
-  console.log(`PriceFeed: ${symbol} price updated.`);
+    .updatePrice(COLLATERAL[process.env.CURRENCY], { gasLimit: 300000 });
+  console.log(`PriceFeed: ${process.env.CURRENCY} price updated.`);
 };
 
 init();
