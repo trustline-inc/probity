48abc525018bce8ff1e2f549adee4a5911d13395
shine <aungshine@gmail.com>
2021-12-01 10:33:41 -0800

    rename price -> adjustedPrice

--- contracts/probity/VaultEngine.sol
+++ contracts/probity/VaultEngine.sol
@@ -31,7 +31,7 @@ contract VaultEngine is Stateful, Eventful {
     struct Collateral {
         uint256 debtAccumulator; // Cumulative debt rate
         uint256 capitalAccumulator; // Cumulative capital rate
-        uint256 price; // Price adjusted for collateral ratio
+        uint256 adjustedPrice; // Price adjusted for collateral ratio
         uint256 normDebt; // Normalized debt
         uint256 normCapital; // Normalized supply
         uint256 ceiling; // Max. amount that can be supplied/borrowed
@@ -412,8 +412,8 @@ contract VaultEngine is Stateful, Eventful {
      * @param price The new price
      */
     function updatePrice(bytes32 collId, uint256 price) external onlyByRegistered {
-        emit LogVarUpdate("Vault", collId, "price", collateralTypes[collId].price, price);
-        collateralTypes[collId].price = price;
+        emit LogVarUpdate("Vault", collId, "price", collateralTypes[collId].adjustedPrice, price);
+        collateralTypes[collId].adjustedPrice = price;
     }
 
     /////////////////////////////////////////
@@ -428,7 +428,7 @@ contract VaultEngine is Stateful, Eventful {
     function certify(bytes32 collId, Vault memory vault) internal view {
         require(
             (vault.debt * collateralTypes[collId].debtAccumulator) + (vault.capital * PRECISION_PRICE) <=
-                vault.usedCollateral * collateralTypes[collId].price,
+                vault.usedCollateral * collateralTypes[collId].adjustedPrice,
             "Vault/certify: Not enough collateral"
         );
     }
