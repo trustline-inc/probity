fd2dd3568599523243e5fd896333426c24a498d5
Matthew Rosendin <matt@trustline.co>
2022-03-08 17:25:10 -0800

    wrap in try-catch

--- scripts/initialize.ts
+++ scripts/initialize.ts
@@ -68,96 +68,100 @@ const init = async () => {
     owner
   );
 
-  // Allows Trustline. An address can only have one role at a time.
-  // Note: governance address was set at deployment
-  console.log(`Allowing address: ${TRUSTLINE_INC}`);
-  let tx = await registry
-    .connect(owner)
-    .setupAddress(
-      ethers.utils.formatBytes32String("whitelisted"),
-      TRUSTLINE_INC,
-      {
-        gasLimit: 300000,
-      }
-    );
-  await tx.wait();
+  try {
+    // Allows Trustline. An address can only have one role at a time.
+    // Note: governance address was set at deployment
+    console.log(`Allowing address: ${TRUSTLINE_INC}`);
+    let tx = await registry
+      .connect(owner)
+      .setupAddress(
+        ethers.utils.formatBytes32String("whitelisted"),
+        TRUSTLINE_INC,
+        {
+          gasLimit: 300000,
+        }
+      );
+    await tx.wait();
 
-  // Initialize the native token in VaultEngine
-  console.log(`Initializing ${nativeToken} asset`);
-  tx = await vaultEngine
-    .connect(owner)
-    .initAsset(ASSETS[nativeToken], { gasLimit: 400000 });
-  await tx.wait();
-  console.log(`Vault: ${nativeToken} initialized`);
-
-  // Limit songbird vault to 1000 AUR
-  if (networkName === "songbird") {
-    const limit = 1000;
+    // Initialize the native token in VaultEngine
+    console.log(`Initializing ${nativeToken} asset`);
     tx = await vaultEngine
       .connect(owner)
-      .updateIndividualVaultLimit(RAD.mul(limit), {
+      .initAsset(ASSETS[nativeToken], { gasLimit: 400000 });
+    await tx.wait();
+    console.log(`Vault: ${nativeToken} initialized`);
+
+    // Limit songbird vault to 1000 AUR
+    if (networkName === "songbird") {
+      const limit = 1000;
+      tx = await vaultEngine
+        .connect(owner)
+        .updateIndividualVaultLimit(RAD.mul(limit), {
+          gasLimit: 300000,
+        });
+      await tx.wait();
+      console.log(`Vault: individual limit set to ${limit} ${nativeToken}`);
+    }
+
+    // Update vault debt ceiling to 10M AUR
+    const ceiling = 10_000_000;
+    tx = await vaultEngine
+      .connect(owner)
+      .updateCeiling(ASSETS[nativeToken], RAD.mul(ceiling), {
         gasLimit: 300000,
       });
     await tx.wait();
-    console.log(`Vault: individual limit set to ${limit} ${nativeToken}`);
+    console.log(`Vault: ceiling updated to ${ceiling} ${nativeToken}`);
+
+    // Update vault debt floor to 1 AUR
+    const floor = 1;
+    tx = await vaultEngine
+      .connect(owner)
+      .updateFloor(ASSETS[nativeToken], WAD.mul(floor), {
+        gasLimit: 300000,
+      });
+    await tx.wait();
+    console.log(`Vault: floor updated to ${floor} ${nativeToken}`);
+
+    // Initialize native token in Teller (sets default protocol fee)
+    tx = await teller
+      .connect(owner)
+      .initAsset(ASSETS[nativeToken], 0, { gasLimit: 300000 });
+    await tx.wait();
+    console.log(`Teller: ${nativeToken} initialized`);
+
+    // Initialize native token in Liquidator (sets default penalty fees)
+    tx = await liquidator
+      .connect(owner)
+      .initAsset(ASSETS[nativeToken], process.env.AUCTIONEER, {
+        gasLimit: 300000,
+      });
+    await tx.wait();
+    console.log(`Liquidator: ${nativeToken} initialized`);
+
+    // Initialize native token price feed
+    const liqRatio = WAD.mul(15).div(10);
+    tx = await priceFeed
+      .connect(owner)
+      .initAsset(ASSETS[nativeToken], liqRatio, process.env.FTSO, {
+        gasLimit: 300000,
+      });
+    await tx.wait();
+    console.log(
+      `PriceFeed: ${nativeToken} price initialized with ${ethers.utils
+        .formatEther(liqRatio)
+        .toString()} liq. ratio`
+    );
+
+    // Fetch native token price
+    tx = await priceFeed
+      .connect(owner)
+      .updateAdjustedPrice(ASSETS[nativeToken], { gasLimit: 300000 });
+    await tx.wait();
+    console.log(`PriceFeed: ${nativeToken} price updated`);
+  } catch (error) {
+    console.log(error);
   }
-
-  // Update vault debt ceiling to 10M AUR
-  const ceiling = 10_000_000;
-  tx = await vaultEngine
-    .connect(owner)
-    .updateCeiling(ASSETS[nativeToken], RAD.mul(ceiling), {
-      gasLimit: 300000,
-    });
-  await tx.wait();
-  console.log(`Vault: ceiling updated to ${ceiling} ${nativeToken}`);
-
-  // Update vault debt floor to 1 AUR
-  const floor = 1;
-  tx = await vaultEngine
-    .connect(owner)
-    .updateFloor(ASSETS[nativeToken], WAD.mul(floor), {
-      gasLimit: 300000,
-    });
-  await tx.wait();
-  console.log(`Vault: floor updated to ${floor} ${nativeToken}`);
-
-  // Initialize native token in Teller (sets default protocol fee)
-  tx = await teller
-    .connect(owner)
-    .initAsset(ASSETS[nativeToken], 0, { gasLimit: 300000 });
-  await tx.wait();
-  console.log(`Teller: ${nativeToken} initialized`);
-
-  // Initialize native token in Liquidator (sets default penalty fees)
-  tx = await liquidator
-    .connect(owner)
-    .initAsset(ASSETS[nativeToken], process.env.AUCTIONEER, {
-      gasLimit: 300000,
-    });
-  await tx.wait();
-  console.log(`Liquidator: ${nativeToken} initialized`);
-
-  // Initialize native token price feed
-  const liqRatio = WAD.mul(15).div(10);
-  tx = await priceFeed
-    .connect(owner)
-    .initAsset(ASSETS[nativeToken], liqRatio, process.env.FTSO, {
-      gasLimit: 300000,
-    });
-  await tx.wait();
-  console.log(
-    `PriceFeed: ${nativeToken} price initialized with ${ethers.utils
-      .formatEther(liqRatio)
-      .toString()} liq. ratio`
-  );
-
-  // Fetch native token price
-  tx = await priceFeed
-    .connect(owner)
-    .updateAdjustedPrice(ASSETS[nativeToken], { gasLimit: 300000 });
-  await tx.wait();
-  console.log(`PriceFeed: ${nativeToken} price updated`);
 };
 
 init();
