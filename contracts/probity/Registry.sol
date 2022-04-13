377ec9aeb73b72a9e9a3f8322c112c8f2a4e1a18
shine <aungshine@gmail.com>
2022-04-13 11:44:09 -0700

    - use openzeppelin's package for erc20 token - remove unused interfaces and dependencies

--- contracts/probity/Registry.sol
+++ contracts/probity/Registry.sol
@@ -2,12 +2,10 @@
 
 pragma solidity ^0.8.0;
 
-import "../interfaces/IRegistry.sol";
-
 /**
  * @notice Stores contract addresses.
  */
-contract Registry is IRegistry {
+contract Registry {
     /////////////////////////////////////////
     // State Variables
     /////////////////////////////////////////
@@ -37,22 +35,22 @@ contract Registry is IRegistry {
     /////////////////////////////////////////
     // External Functions
     /////////////////////////////////////////
-    function setupAddress(bytes32 name, address addr) external override onlyByGov {
+    function setupAddress(bytes32 name, address addr) external onlyByGov {
         addressToName[addr] = name;
         emit ContractAdded(name, addr);
     }
 
-    function removeAddress(address addr) external override onlyByGov {
+    function removeAddress(address addr) external onlyByGov {
         bytes32 name = addressToName[addr];
         addressToName[addr] = bytes32("");
         emit ContractRemoved(name, addr);
     }
 
-    function checkValidity(bytes32 name, address addr) external view override returns (bool) {
+    function checkValidity(bytes32 name, address addr) external view returns (bool) {
         return addressToName[addr] == name;
     }
 
-    function checkValidity(address addr) external view override returns (bool) {
+    function checkValidity(address addr) external view returns (bool) {
         return addressToName[addr] != bytes32("");
     }
 }
