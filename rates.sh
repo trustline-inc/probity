95791695d32665100d32004e21026e84c4f53165
Matthew Rosendin <matt@trustline.co>
2022-08-15 10:49:06 -0700

    Updates (#357)
    * update readme
    
    * add units
    
    * add test for issue #354
    
    * update rates to use 31556926 seconds in a year
    
    Co-authored-by: Matthew Rosendin <mrosendin@linqto.com>

--- rates.sh
+++ rates.sh
@@ -5,7 +5,7 @@ for i in {0..400}
 do
   RAY=1000000000000000000000000000
   APR=$(echo "$i * 0.25" | bc -l)
-  MPR=$(bc -l <<< 'scale=28; e(l(('$APR'/100)+1)/31557600)')
+  MPR=$(bc -l <<< 'scale=28; e(l(('$APR'/100)+1)/31556926)')
   MPR_INT=$(bc -l <<< 'scale=27; ('$MPR' * '$RAY')')
   MPR_INT=$(bc -l <<< 'scale=0; '$MPR_INT'/1')
   APR_INT=$(bc -l <<< 'scale=4; (1 + ('$APR' / 100)) * '$RAY'')
